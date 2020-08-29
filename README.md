0698c7c058187be65d605b9fd890f68e2d0f70ba
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2020-08-29 21:32:21 +0200

    📝 🔨 Add postinstall script for submodule

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # desktop
 schoolsyst's desktop app
+
+## How do I run this app locally ?
+
+For now, schoolsyst runs on a tiny VPS, and the API is therefore limited to requests from `*.schoolsyst.com`. If you want to run this desktop app locally, you may want to also run the API, explained on [the repo's README](https://github.com/schoolsyst/backend/tree/master/README.md).
+
+Here's how to install the desktop app:
+
+1. Clone the repo and `cd` into it
+
+```bash
+git clone https://github.com/schoolsyst/desktop.git
+cd desktop
+```
+
+2. Install NodeJS, version `13.5.0`.
+_The installation procedure changes depending on your platform, head over to [NodeJS's website](https://nodejs.org/)_
+
+3. Install the project's dependencies
+
+```bash
+yarn # Using yarn, or
+npm install # Using npm, or
+pnpm install # Using pnpm, very recomanded one
+```
+
+4. Complete the interactive setup launched by installation
+
+5. Start the app
+```bash
+yarn start # Using yarn, or
+npm run start # Using npm, or
+pnpm start # Using pnpm, very recomanded one
+```
+
+This will compile the webapp and launch the desktop app.
---
+++ setup.js
@@ -0,0 +1,48 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const inquirer =  require('inquirer');
+const { exec } = require('child_process')
+
+clear(); // clear the actual terminal
+
+console.log(
+	chalk.blueBright(
+		figlet.textSync('SchoolSyst', { font: "Small Slant", horizontalLayout: 'full' })
+	)
+);
+
+console.log(
+	chalk.grey(
+	"--------------------------------------------------------------------------------"
+  )
+);
+
+let moduleManager = {
+	type: 'list',
+	name: 'moduleManager',
+	message: 'Select the module manager for node.js you wish want to use (will be used to install depandancy for submodule) : ',
+	choices: ["NPM", "YARN", "PNPM"],
+	default: "NPM"
+};
+
+inquirer
+	.prompt([
+		moduleManager
+	 ])
+	.then(answers => {
+		console.log(chalk.green('>'), chalk.white(`Launching installation with :`), chalk.cyan(answers.moduleManager));		
+		exec(`git pull --recurse-submodules && cd ${__dirname}/webapp && ${answers.moduleManager} install`, (error, stdout) => {
+			if (error) {
+			  console.error(chalk.redBright('x'), `error while setuping submodule, error: ${error}`);
+			}
+			console.log(chalk.green('>'), chalk.white(stdout));
+		});
+  	})
+  	.catch(error => {
+		if(error.isTtyError) {
+		  console.error("Couldn't prompt setup because your environement couldn't render the prompt !")
+		} else {
+		  console.error("Error with the prompt : ", error)
+		}
+  	});
