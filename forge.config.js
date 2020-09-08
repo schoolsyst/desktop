377febce7081aa66b5c0779ce7db36ffc1e62b43
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2020-09-08 21:22:22 +0200

    🔨 move from electron-builder to electron-forge

---
+++ forge.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+module.exports = {
+    make_targets: {
+        win32: [
+            "squirrel"
+        ],
+        darwin: [
+            "zip"
+        ],
+        linux: [
+            "deb",
+            "rpm"
+        ]
+    },
+    electronPackagerConfig: {
+        icon: path.join(__dirname, "design/logo/app-icon.png"),
+        packageManager: "yarn",
+        asar: true,
+        ignore: [
+            ".git(ignore|modules)",
+            "(webapp)\/(?!dist)\\S+",
+            "license",
+            "nodemon.config.json", 
+            "README.md", 
+            "setup.js"
+        ]
+    },
+    electronWinstallerConfig: {
+        name: "schoolsyst-desktop",
+        iconUrl: "https://raw.githubusercontent.com/schoolsyst/design/master/logo/app-icon.ico",
+        setupIcon: path.join(__dirname, "design/logo/app-icon.ico"),
+        loadingGif: path.join(__dirname, "design/logo-animations/loading-overlay.gif")
+    },
+    electronInstallerDebian: {},
+    electronInstallerRedhat: {},
+    github_repository: {
+        owner: "schoolsyst",
+        name: "desktop"
+    },
+    windowsStoreConfig: {
+        packageName: "com.schoolsyst.desktop",
+        name: "schoolsyst-desktop"
+    }
+}
