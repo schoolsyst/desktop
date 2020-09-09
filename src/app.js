5f4d308e3601611c04423cb6f6727ab54348a731
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2020-09-09 21:11:35 +0200

    ✨ Add autoUpdate

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, nativeImage } = require('electron');
+const { app, BrowserWindow, nativeImage, autoUpdater, dialog } = require('electron');
 const { createMainWindow } = require('./electron/window')
 const { createTray } = require("./electron/tray");
 const server = require('./express/server');
@@ -6,6 +6,39 @@ const path = require('path');
 
 if(require('electron-squirrel-startup')) app.quit(); // if we need of squirel
 
+/***************
+** AutoUpdate **
+****************/
+if (process.env.NODE_ENV != "dev") {
+	const updateServer = 'http://desktop.schoolsyst.com';
+	const updateUrl = `${updateServer}/update/${process.platform}/${app.getVersion()}`;
+
+	autoUpdater.setFeedURL({ updateUrl });
+	setInterval(() => {autoUpdater.checkForUpdates()}, 60000);
+
+	autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+		const dialogOpts = {
+		type: 'info',
+		buttons: ['Restart', 'Later'],
+		title: 'Application Update',
+		message: process.platform === 'win32' ? releaseNotes : releaseName,
+		detail: `A new version has been downloaded. Redémarrez l'application pour appliquer les mises à jour.`
+		}
+	
+		dialog.showMessageBox(dialogOpts).then((returnValue) => {
+		if (returnValue.response === 0) autoUpdater.quitAndInstall();
+		});
+	});
+
+	autoUpdater.on('error', message => {
+		console.error('There was a problem updating the application');
+		console.error(message);
+	});
+}
+
+/************
+** Express **
+*************/
 server.start(path.normalize(`${__dirname}/../webapp/dist`), process.env.PORT ? process.env.PORT : 8080);
 const _URL_ = `http://localhost:${process.env.PORT ? process.env.PORT : 8080}`;
 const icon = nativeImage.createFromPath(path.join(__dirname, '../design/logo/app-icon.png')).resize({height: 500,width:500});
